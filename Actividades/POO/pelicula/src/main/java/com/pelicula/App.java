d8387605b4467a9b2e35977c9f33ec2243d580ce
Edyaz17 <142721266+Edyaz17@users.noreply.github.com>
2023-09-26 20:41:25 -0600

    Delete Actividades/POO directory
    Lo elimino porque lo subí directamente a main

--- Actividades/POO/pelicula/src/main/java/com/pelicula/App.java
+++
@@ -1,9 +0,0 @@
-package com.pelicula;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Proyecto java: GESTION DE PELICULAS" );
-    }
-}
--- Actividades/POO/pelicula/src/main/java/com/pelicula/GestorPelicula.java
+++
@@ -1,5 +0,0 @@
-package com.pelicula;
-
-public class GestorPelicula {
-    
-}
